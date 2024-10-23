93534688c0996e929b17434644f33c7e7f5f6053
McleanServices <tyrecemcleanterence@gmail.com>
2024-10-23 12:41:13 -0400

    Your commit message

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -11,12 +11,15 @@ app.use(cors({
 
 
 // Simple GET endpoint
-app.get('/api/test', (req, res) => {
-  const response = { message: 'Hello from the Express API!', timestamp: new Date() };
-  console.log('Sending Response:', response);
-  res.json(response);
-});
+// app.get('/api/test', (req, res) => {
+//   const response = { message: 'Hello from the Express API!', timestamp: new Date() };
+//   console.log('Sending Response:', response);
+//   res.json(response);
+// });
 
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from the Express API!', timestamp: new Date() });
+});
 
 // Start the server
 app.listen(port, '0.0.0.0', () => {
